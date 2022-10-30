54a5bad609e0ceb1d35c4a5c13047a830b260f4e
chenshuhong <1573391005@qq.com>
2022-10-31 02:50:26 +0800

    值类型文档

---
+++ test/Int.js
@@ -0,0 +1,21 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Int", function () {
+  async function deployFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Int = await ethers.getContractFactory("Int");
+    const int = await Int.deploy();
+    return { int, owner, otherAccount };
+  }
+  it("contraryBit", async function () {
+    const { int, owner, otherAccount } =
+      await loadFixture(deployFixture);
+
+    expect(await int.contraryBit(0)).to.be;
+  });
+
+});
